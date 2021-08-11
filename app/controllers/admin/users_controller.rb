09417a0981ed562a3147b9dc536f912151a18299
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-08-11 15:01:34 +0000

    [Add]詳細ページ機能の作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,9 +5,20 @@ class Admin::UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.suspended = true
+    @user.update(user_params)
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.permit(:suspended)
   end
 
 end
