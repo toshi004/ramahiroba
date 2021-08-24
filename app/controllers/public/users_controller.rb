8dce030cb9d41bb296a75c461de7428b284a810d
ToshikiIwabuchi <d.toshi004.35@gmail.com>
2021-08-24 15:06:43 +0000

    [Add]ユーザー一覧のレイアウト

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,12 +1,12 @@
 class Public::UsersController < ApplicationController
 
   def index
-    if path[:controller] == "public/users" && path[:action] == "my_page"
-      user = User.find(params[:user_id])
-      @users = user.followings.page(params[:page]).per(8)
-    else
-      @users = User.page(params[:page]).per(8)
-    end
+    # if path[:controller] == "public/users" && path[:action] == "my_page"
+      # user = User.find(params[:user_id])
+      # @users = user.followings.page(params[:page]).per(8)
+    # else
+      @users = User.page(params[:page]).per(6)
+    # end
   end
 
   def show
